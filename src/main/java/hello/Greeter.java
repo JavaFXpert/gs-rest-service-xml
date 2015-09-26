3adbbe354cc39ae56235f8ae693c3b932edef467
JavaFXpert <james@j1w.xyz>
2015-09-26 07:58:13 -0400

    Added class Greeter to demonstrate serializing graph of objects

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -3,13 +3,11 @@ package hello;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
-@JsonRootName("salutation")
 public class Greeter {
 
     @JsonProperty("sender-name")
     private final String name;
 
-    @JsonProperty("year-sent")
     private final int year;
 
     public Greeter(String name, int year) {
